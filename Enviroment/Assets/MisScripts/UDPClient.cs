cfa3ca19ff34d284d620cccd92eb80c9955aaad7
Saul Gausin <saul.gausin@gmail.com>
2014-05-15 22:47:14 -0500

    Add testing messages to react in the game by sendig the word 'camina' to move the player in the game and 'nada' to do nothing in the game

--- Enviroment/Assets/MisScripts/UDPClient.cs
+++ Enviroment/Assets/MisScripts/UDPClient.cs
@@ -11,81 +11,35 @@ using System.Net;
 
 public class UDPClient : MonoBehaviour {
 	//thread object
-	Thread receiverThread;
+	private Thread receiverThread;
 	//udpclient object
-	UdpClient client;
+	private UdpClient client;
 	
 	//port to listen
-	public int port = 1600;
+	public static int port = 1600;
 	//ip address to listen
-	public string ipAddress = "127.0.0.1";
-	//text field to set the port
-	public string portTxtField;
+	public static string ipAddress = "127.0.0.1";
 	//the current message received
-	public string currentMessage;
+	private string currentMessage;
 	//the last message received
-	public string lastMessageReceived;
+	private string lastMessageReceived;
 
 	public Boolean camina_usuario;
-	
+
 	//this function is infinity loop
 	void Update(){
-		//to avoid duplicate message received and just
-		//change the text object in case the message is different
-		//if (!lastMessageReceived.Equals(currentMessage)) {
-		//
-		//if (lastMessageReceived == "asap") {
-		//	camina_usuario = true;
-		//	Debug.Log (lastMessageReceived.ToString ());
-
-		//} else {
-	//		camina_usuario = false;
-	//	}
-		//output_txt.text = lastMessageReceived;
-		//Debug.Log(output_txt.text);
-		currentMessage = lastMessageReceived;
-		//}
+		if (lastMessageReceived == "camina") {
+			camina_usuario = true;
+			Debug.Log (lastMessageReceived);
+		} else {
+			camina_usuario = false;
+			Debug.Log (lastMessageReceived);
+		}
 	}
 
 	void Start(){
 		init();
 	}
-	/*
-	//Function to setting up the GUI
-	void OnGUI(){
-		//Starting the string with default message
-		//lastMessageReceived = "No message received yet.";
-		//set the default message in the object text
-		//output_txt.text = lastMessageReceived;
-		//set the default message in the current message
-		//currentMessage = lastMessageReceived;
-		
-		//Set the GUI horizontal to add the components
-		GUILayout.BeginHorizontal();
-		//add a label with Ip address as static text
-		GUILayout.Label("Ip address: ");
-		//Add a text field with the ipAddress
-		ipAddress = GUILayout.TextField(ipAddress);
-		//Add a label with port as static text
-		GUILayout.Label("Port: ");
-		//add a text field with the port
-		portTxtField = GUILayout.TextField(port.ToString());
-		
-		//In case of click Start Listening button 
-		if(GUILayout.Button("Start listening")){
-			//Print out the ip address and current port setted
-			print("It's about to connect to this ip Address:"+ipAddress+" and port: "+portTxtField);
-			//call init function
-
-		}
-		//In case of click disconnect
-		if(GUILayout.Button("Disconnect")){
-			//kill the thread added
-			stopListenning();
-		}
-	}
-	*/
-	
 	//Initialize the thread to run in background
 	void init(){
 		//debug
@@ -112,8 +66,6 @@ public class UDPClient : MonoBehaviour {
 	private void updateReceivedData(){
 		//Debug
 		print("Starting updating data");
-		//Parse the port from the text field of port
-		port = int.Parse (portTxtField);
 		//Initialize UDPClient with the port parsed
 		client = new UdpClient(port);
 		//Just declaring the byte array
@@ -121,20 +73,12 @@ public class UDPClient : MonoBehaviour {
 		//infinite loop
 		while(true){
 			try {
-				//debug
-				print("Updating data");
 				//initilizing the end point object which contains the ipaddress and port to listen
 				IPEndPoint ipEndPointObj = new IPEndPoint(IPAddress.Parse(ipAddress), port);
-				//debug
-				print("After IPEndPoint");
 				//Received the data from the ipendpoint object
 				data = client.Receive(ref(ipEndPointObj));
-				//debug
-				print("Taking message");
 				//Getting the data from the bytes array
 				lastMessageReceived =  System.Text.Encoding.ASCII.GetString(data, 0, data.Length);
-				//debug
-				//print(lastMessageReceived);
 				//To handle errors
 			} catch (Exception e){
 				//debug
